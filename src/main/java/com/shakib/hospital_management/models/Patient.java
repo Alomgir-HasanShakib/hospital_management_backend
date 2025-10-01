dcdc6fc77d1b5d42fbeb597e6af945c11c0ee6df
Alomgir Hasan Shakib <alomgirhasanshakib@gmail.com>
2025-10-01 18:39:10 +0600

    created patient model

--- src/main/java/com/shakib/hospital_management/models/Patient.java
+++ src/main/java/com/shakib/hospital_management/models/Patient.java
@@ -1,6 +1,21 @@
 package com.shakib.hospital_management.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String gender;
